793dac757bba390fe9ba8e5dd3636ff75b1197a1
donlanA <1234littleduck@gmail.com>
2025-08-25 23:03:54 +0800

    debug test

--- index.js
+++ index.js
@@ -1,64 +1,61 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-// Discord Bot
-const { Client, GatewayIntentBits } = require('discord.js');
+// // Discord Bot
+// const { Client, GatewayIntentBits } = require('discord.js');
 
-const discordClient = new Client({
-    intents:
-        [
-            // 基本事件
-            GatewayIntentBits.Guilds, 
+// const discordClient = new Client({
+//     intents:
+//         [
+//             // 基本事件
+//             GatewayIntentBits.Guilds, 
 
-            // 排程事件
-            GatewayIntentBits.GuildScheduledEvents, 
+//             // 排程事件
+//             GatewayIntentBits.GuildScheduledEvents, 
 
-            // 聊天室如果有動作的事件
-            GatewayIntentBits.GuildMessages, 
+//             // 聊天室如果有動作的事件
+//             GatewayIntentBits.GuildMessages, 
 
-            // 接收聊天室內容的事件 → 需要到 Discord Developer Portal 把 MESSAGE CONTENT INTENT 打開
-            GatewayIntentBits.MessageContent, 
+//             // 接收聊天室內容的事件 → 需要到 Discord Developer Portal 把 MESSAGE CONTENT INTENT 打開
+//             GatewayIntentBits.MessageContent, 
 
-            // 接收到反應的事件
-            GatewayIntentBits.GuildMessageReactions 
-        ]
-});
+//             // 接收到反應的事件
+//             GatewayIntentBits.GuildMessageReactions 
+//         ]
+// });
 
+// discordClient.on('ready', () => {
+//     // 設定我們的機器人狀態
+//     // activities 的 name 是狀態的名稱
+//     // status 是表示在線上或者忙碌中 目前是 online 也就是常常看到的綠燈
+//     discordClient.user.setPresence({ activities: [{ name: '百日戰記 -最終防衛學園-' }], status: 'online' });
+// });
 
+// // 如果接收到新訊息
+// discordClient.on('messageCreate', async msg => {
 
+//     // 不要接收機器人的訊息
+//     if (msg.author.bot)
+//         return;
 
-discordClient.on('ready', () => {
-    // 設定我們的機器人狀態
-    // activities 的 name 是狀態的名稱
-    // status 是表示在線上或者忙碌中 目前是 online 也就是常常看到的綠燈
-    discordClient.user.setPresence({ activities: [{ name: '百日戰記 -最終防衛學園-' }], status: 'online' });
-});
+//     // 這裡 rplyToken 傳 null，inputStr 傳 msg.content
+//     let reply = parseInput(null, msg.content);
 
-// 如果接收到新訊息
-discordClient.on('messageCreate', async msg => {
-
-    // 不要接收機器人的訊息
-    if (msg.author.bot)
-        return;
-
-    // 這裡 rplyToken 傳 null，inputStr 傳 msg.content
-    let reply = parseInput(null, msg.content);
-
-    // 如果有的話 reply 的內容一定大於 0
-    if (reply.length > 0) {
-        // 讓機器人回話
-        if (Array.isArray(reply)) {
-            // 只回傳第一個 type: "text" 的內容
-            let textMsg = reply.find(m => m.type === "text");
-            if (textMsg) msg.reply(textMsg.text);
-        } else if (typeof reply === 'string') {
-            msg.reply(reply);
-        }
-    }
-});
+//     // 如果有的話 reply 的內容一定大於 0
+//     if (reply.length > 0) {
+//         // 讓機器人回話
+//         if (Array.isArray(reply)) {
+//             // 只回傳第一個 type: "text" 的內容
+//             let textMsg = reply.find(m => m.type === "text");
+//             if (textMsg) msg.reply(textMsg.text);
+//         } else if (typeof reply === 'string') {
+//             msg.reply(reply);
+//         }
+//     }
+// });
 
 
-// Discord Developer Portal 上 Bot 的 Token
-discordClient.login(process.env.DISCORD_TOKEN);
+// // Discord Developer Portal 上 Bot 的 Token
+// discordClient.login(process.env.DISCORD_TOKEN);
 
 //------------------------------
 // Line Bot
